f46c29d46ff9fbeb7e5b7e2586f34e4b7855153d
Henrique Hartwig <henrique2805@gmail.com>
2025-05-08 00:26:54 -0300

    fix: update database outputs to reference the payment microservice instead of the order microservice

--- terraform/modules/database/outputs.tf
+++ terraform/modules/database/outputs.tf
@@ -1,9 +1,9 @@
 output "db_instance_endpoint" {
-  value       = aws_db_instance.db_fast_food_orders.endpoint
+  value       = aws_db_instance.db_fast_food_payments.endpoint
   description = "Database endpoint"
 }
 
 output "db_instance_address" {
-  value       = aws_db_instance.db_fast_food_orders.address
+  value       = aws_db_instance.db_fast_food_payments.address
   description = "Database address"
 }
