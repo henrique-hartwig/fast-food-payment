0c568ea615efabe7c0367feaeb0067a64962329d
Henrique Hartwig <henrique2805@gmail.com>
2025-05-08 00:14:04 -0300

    chore: update Lambda Layer zip file naming and description in pipeline workflow and Terraform configuration

--- terraform/modules/lambda_layer/main.tf
+++ terraform/modules/lambda_layer/main.tf
@@ -1,7 +1,7 @@
 resource "aws_lambda_layer_version" "lambda_layer" {
-  filename   = "${path.module}/fastfood-lambda-layer.zip"
-  layer_name = "fastfood-lambda-layer"
+  filename   = "${path.module}/fastfood-payment-lambda-layer.zip"
+  layer_name = "fastfood-payment-lambda-layer"
   compatible_runtimes = ["nodejs18.x"]
   skip_destroy = true
-  description = "Fast Food Lambda Layer"
+  description = "Fast Food Payment Lambda Layer"
 }
